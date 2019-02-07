34943f8937165c54261581017a1a750363e11c76
Sean Kelly <shonkelli@gmail.com>
2019-02-07 00:44:23 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  index = 0
+  matched = true
+  while matched == true do
+    collection = []
+    arr.each { |phrase| collection << phrase[index] }
+    unique_letters = collection.uniq
+    unique_letters.length > 1 ? matched = false : index += 1
+  end
+  arr.sort_by {|word| alphabet.index(word[index])}
 end
