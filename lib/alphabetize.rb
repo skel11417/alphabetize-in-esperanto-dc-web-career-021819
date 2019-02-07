e58ad5000303d4052fc237f51a9d2488dc2261a1
Sean Kelly <shonkelli@gmail.com>
2019-02-07 00:27:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,24 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  index = 0
+  matched_array = true
+  while matched_array == true do 
+    a = arr.sort_by {|word| alphabet.index(word[index])}
+    b = arr.sort_by {|word| alphabet.index(word[index + 1])}
+    puts "Compare\n#{a}\n#{b} on index #{index}"
+    if a != b
+      matched_array = false
+    else
+      index += 1
+    end
+  end
+  puts "Sorting on index #{index}"
+  puts "----"
+  arr.sort_by {|word| alphabet.index(word[index])}
 end
+
+  greetings = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+  sorted_expressions = ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
+
+puts alphabetize(greetings)
