136c148d3db30b5147d69cd0255dc28c8b911a06
Sean Kelly <shonkelli@gmail.com>
2019-02-07 00:44:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -11,8 +11,3 @@ def alphabetize(arr)
   end
   arr.sort_by {|word| alphabet.index(word[index])}
 end
-
-  greetings = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
-  sorted_expressions = ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
-
-puts alphabetize(greetings)
